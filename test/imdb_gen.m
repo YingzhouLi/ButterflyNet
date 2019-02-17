babecb4338b65f5cdc821fdb057786663c0060db
YingzhouLi <yingzhouli0417@gmail.com>
2019-02-17 11:39:29 -0500

    Backup output results

--- test/imdb_gen.m
+++ test/imdb_gen.m
@@ -2,8 +2,8 @@ N = 2^6;
 n_train = 25600;
 n_test = 100;
 
-smooth = 0;
-lfreq = 1;
+smoothlvl = 0; % 0 non smooth, 1 pos non smooth 2 smooth, 3 pos smooth
+lfreq = 0;
 seqdata = 1;
 
 xx = ((0:N-1)/N)';
@@ -24,15 +24,24 @@ end
 
 DFT = funFT(kk,xx);
 
-if ~smooth
+if smoothlvl == 3
+    [x_train,y_train] = rand_real_possmooth(DFT,n_train,kk);
+    [x_test,y_test] = rand_real_possmooth(DFT,n_test,kk);
+elseif smoothlvl == 2
+    [x_train,y_train] = rand_real_smooth(DFT,n_train,kk);
+    [x_test,y_test] = rand_real_smooth(DFT,n_test,kk);
+elseif smoothlvl == 1
     x_train = rand(n_train,N);
     y_train = x_train*DFT';
     
     x_test = rand(n_test,N);
     y_test = x_test*DFT';
 else
-    [x_train,y_train] = rand_real_smooth(DFT,n_train,kk);
-    [x_test,y_test] = rand_real_smooth(DFT,n_test,kk);
+    x_train = randn(n_train,N);
+    y_train = x_train*DFT';
+    
+    x_test = randn(n_test,N);
+    y_test = x_test*DFT';
 end
 
 % Normalization
@@ -47,20 +56,32 @@ y_test = y_test./(y_norm*ones(1,size(y_test,2)));
 y_train = comp2real(y_train);
 y_test = comp2real(y_test);
 
-if ~smooth
+if smoothlvl == 3
     if lfreq
-        fname = 'data_DFT.mat';
+        fname = 'data_DFT_possmooth.mat';
     else
-        fname = 'data_DFT_hfreq.mat';
+        fname = 'data_DFT_possmooth_hfreq.mat';
     end
-else
+elseif smoothlvl == 2
     if lfreq
         fname = 'data_DFT_smooth.mat';
     else
         fname = 'data_DFT_smooth_hfreq.mat';
     end
+elseif smoothlvl == 2
+    if lfreq
+        fname = 'data_DFT_pos.mat';
+    else
+        fname = 'data_DFT_pos_hfreq.mat';
+    end
+else
+    if lfreq
+        fname = 'data_DFT.mat';
+    else
+        fname = 'data_DFT_hfreq.mat';
+    end
 end
-save(fname, 'n_train', 'n_test', 'in_siz', 'in_range', ...
+save(['data/' fname], 'n_train', 'n_test', 'in_siz', 'in_range', ...
     'out_siz', 'out_range', ...
     'x_train', 'y_train', 'x_test', 'y_test', '-v7');
 
@@ -70,23 +91,35 @@ if seqdata
     tot_y_train = y_train;
     initsiz = 100;
     while initsiz <= tot_n_train
-        if ~smooth
+        if smoothlvl == 3
             if lfreq
-                fname = ['data_DFT_' sprintf('%06d',initsiz) '.mat'];
+                fname = ['data_DFT_possmooth_' sprintf('%06d',initsiz) '.mat'];
             else
-                fname = ['data_DFT_hfreq_' sprintf('%06d',initsiz) '.mat'];
+                fname = ['data_DFT_possmooth_hfreq_' sprintf('%06d',initsiz) '.mat'];
             end
-        else
+        elseif smoothlvl == 2
             if lfreq
                 fname = ['data_DFT_smooth_' sprintf('%06d',initsiz) '.mat'];
             else
                 fname = ['data_DFT_smooth_hfreq_' sprintf('%06d',initsiz) '.mat'];
             end
+        elseif smoothlvl == 1
+            if lfreq
+                fname = ['data_DFT_pos_' sprintf('%06d',initsiz) '.mat'];
+            else
+                fname = ['data_DFT_pos_hfreq_' sprintf('%06d',initsiz) '.mat'];
+            end
+        else
+            if lfreq
+                fname = ['data_DFT_' sprintf('%06d',initsiz) '.mat'];
+            else
+                fname = ['data_DFT_hfreq_' sprintf('%06d',initsiz) '.mat'];
+            end
         end
         n_train = initsiz;
         x_train = tot_x_train(1:n_train,:);
         y_train = tot_y_train(1:n_train,:);
-        save(fname, 'n_train', 'n_test', 'in_siz', 'in_range', ...
+        save(['data/' fname], 'n_train', 'n_test', 'in_siz', 'in_range', ...
             'out_siz', 'out_range', ...
             'x_train', 'y_train', 'x_test', 'y_test', '-v7');
         initsiz = 4*initsiz;
@@ -104,6 +137,26 @@ for itk = 1:lenk
     else
         negk = find(kk == -kk(itk));
         y_data(:,itk) = randn(n,1)+1i*randn(n,1);
+        if ~isempty(negk)
+            y_data(:,negk) = conj(y_data(:,itk));
+        end
+    end
+end
+nzidx = setdiff(1:lenk,zeroidx);
+x_data = real(y_data*DFT+conj(y_data(:,nzidx)*DFT(nzidx,:)))/size(DFT,2);
+end
+
+function [x_data,y_data] = rand_real_possmooth(DFT,n,kk)
+lenk = length(kk);
+y_data = zeros(n,lenk);
+zeroidx = [];
+for itk = 1:lenk
+    if kk(itk) == 0
+        zeroidx = itk;
+        y_data(:,itk) = 2*rand(n,1);
+    else
+        negk = find(kk == -kk(itk));
+        y_data(:,itk) = rand(n,1)+1i*rand(n,1);
         if ~isempty(negk)
             y_data(:,negk) = conj(y_data(:,itk));
         end
--- test/outputs/untitled.m
+++ test/outputs/untitled.m
@@ -1,12 +1,14 @@
-filename = 'DFT_smooth_hfreq_20190114_Bathch1000_Channel8_Samp001600';
+filename = 'DFT_20190208_smooth_Gen_Bathch1000_Channel8_Samp025600';
 fid = fopen([filename '.out'],'r');
-data = textscan(fid, 'Iter # %d: Train Loss: %f; Test Loss: %f.', 'Headerlines', 18);
+%data = textscan(fid, 'Iter # %d: Train Loss: %f; Test Loss: %f.', 'Headerlines', 18);
+data = textscan(fid, 'Iter # %d: Train Loss: %f; Test Loss: %f; Gen Test Loss: %f.', 'Headerlines', 20);
 figure
 semilogy(data{1},data{2});
 title(regexprep(filename, '_', ' '))
 hold on
 semilogy(data{1},data{3});
-ylim([5e-5 1e-1]);
+semilogy(data{1},data{4});
+ylim([1e-4 10]);
 fclose(fid);
 
 %saveas(gcf, filename, 'jpg');
